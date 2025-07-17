ee48851c0a434218ea69c3d464cd6787c16e57a8
Pluem10 <664259010@webmail.npru.ac.th>
2025-07-17 15:55:14 +0700

    Update1

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import restaurantRouter from "./routers/restaurant.router.js";
 import cors from "cors";
 app.use(
   cors({
-    origin: ["http://localhost:5173", "127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -15,15 +15,6 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// เพิ่ม middleware สำหรับ CSP header
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; script-src 'self'; object-src 'none';"
-  );
-  next();
-});
 
 app.get("/", (req, res) => {
   res.send("Restaurant Restful API Test New ");
